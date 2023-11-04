777ea6f5368cda1dacb8c776e2a1992117f83da5
dolto <hdw810@naver.com>
2023-11-04 12:59:32 +0900

    test

---
+++ src/main/java/kr/ac/wku/SpringWorkSpace/SpringWorkSpaceApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.wku.SpringWorkSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWorkSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/wku/SpringWorkSpace/SpringWorkSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.wku.SpringWorkSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorkSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
